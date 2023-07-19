589d2e03e70346b678c03604f4afcf4fb86d9576
Nick Chen <1358390935@qq.com>
2023-07-19 15:02:24 +0800

    feat: 404 page

--- components.d.ts
+++ components.d.ts
@@ -7,6 +7,7 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    404: typeof import('./src/components/404.vue')['default']
     ArticleCard: typeof import('./src/components/ArticleCard.vue')['default']
     BaseLayout: typeof import('./src/layouts/base.vue')['default']
     Island: typeof import('./node_modules/.pnpm/registry.npmmirror.com+iles@0.9.5_sass@1.63.6/node_modules/iles/dist/client/app/components/Island.vue')['default']
@@ -14,5 +15,6 @@ declare module '@vue/runtime-core' {
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     TheFooter: typeof import('./src/components/TheFooter.vue')['default']
+    WaifuCard: typeof import('./src/components/WaifuCard.vue')['default']
   }
 }
---
+++ src/pages/404.vue
@@ -0,0 +1,40 @@
+<template></template>
+<style>
+    html,
+    body {
+        display: block;
+        width: 100%;
+        height: 100%;
+        padding: 0;
+        margin: 0;
+    }
+
+    /* 手机屏幕适应 */
+    @media only screen and (max-width: 600px) {
+        body {
+            background: url('../public/404_hor.png') center no-repeat;
+            background-size: cover;
+        }
+    }
+
+    /* 平板屏幕适应 */
+    @media only screen and (min-width: 601px) and (max-width: 1024px) {
+        body {
+            background: url('../public/404.png') center no-repeat;
+            background-size: cover;
+        }
+    }
+
+    /* 桌面屏幕适应 */
+    @media only screen and (min-width: 1024px) {
+        body {
+            background: url('../public/404.png') center no-repeat;
+            background-size: cover;
+        }
+    }
+
+</style>
+<style lang="scss">
+@import '~/assets/base.css';
+</style>
+
