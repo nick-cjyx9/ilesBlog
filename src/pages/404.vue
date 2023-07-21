34abea2ef26b172d40dd6cb213669569380c1d2f
Nick Chen <1358390935@qq.com>
2023-07-21 21:25:39 +0800

    fix: 404 style leak

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,35 +1,7 @@
-<template></template>
-<style scoped>
-html,
-body {
-    display: block;
-    width: 100%;
-    height: 100%;
-    padding: 0;
-    margin: 0;
-}
-
-/* 手机屏幕适应 */
-@media only screen and (max-width: 600px) {
-    body {
-        background: url('/404_hor.png') center no-repeat;
-        background-size: cover;
-    }
-}
-
-/* 平板屏幕适应 */
-@media only screen and (min-width: 601px) and (max-width: 1024px) {
-    body {
-        background: url('/404.png') center no-repeat;
-        background-size: cover;
-    }
-}
-
-/* 桌面屏幕适应 */
-@media only screen and (min-width: 1024px) {
-    body {
-        background: url('/404.png') center no-repeat;
-        background-size: cover;
-    }
-}
-</style>
+<template>
+</template>
+<script setup lang="ts">
+let style = document.createElement('style');
+style.innerText = `html,body {display: block;width: 100%;height: 100%;padding: 0;margin: 0;}@media only screen and (max-width: 600px) {body {background: url('/404_hor.png') center no-repeat;background-size: cover;}}@media only screen and (min-width: 601px) and (max-width: 1024px) {body {background: url('/404.png') center no-repeat;background-size: cover;}}@media only screen and (min-width: 1024px) {body {background: url('/404.png') center no-repeat;background-size: cover;}}`;
+document.body.appendChild(style);
+</script>
