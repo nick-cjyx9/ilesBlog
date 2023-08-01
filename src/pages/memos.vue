85eb85a9a4e8f58e356fb3692dc73590a31b683a
Nick Chen <1358390935@qq.com>
2023-08-01 15:55:40 +0800

    fix: add client:load

--- src/pages/memos.vue
+++ src/pages/memos.vue
@@ -1,6 +1,6 @@
 <template layout="base">
     <div class="card justify-center p-6 lg:px-20 rounded-xl">
-        <h1 class="text-center font-semibold munderline px-3 text-3xl mt-8" role="button" title="我是标题~"><del>也许是日寄</del></h1>
+        <h1 class="text-center font-semibold munderline px-3 text-3xl mt-8" role="button" title="我是标题~"><del>也许是日寄(加载不出来请刷新)</del></h1>
 
         <div id="memos" class="memos">
         </div>
@@ -14,26 +14,34 @@
         </div>
     </div>
 </template>
+
 <script setup lang="ts">
-const { frontmatter, meta } = usePage();
 useHead({
     link: [
         { type: 'text/css', rel: 'stylesheet', href: '/styles/memos_style.css' },
         { type: 'text/css', rel: 'stylesheet', href: '/styles/APlayer.min.css' },
         { type: 'text/css', rel: 'stylesheet', href: '/styles/highlight.github.min.css' },
         { type: 'text/css', rel: 'stylesheet', href: '/styles/memos_custom.css' },
-    ],
-    script: [
-        { src: '/assets/js/lazyload.min.js' },
-        { src: '/assets/js/marked.min.js' },
-        { src: '/assets/js/view-image.min.js' },
-        { src: '/assets/js/moment.min.js' },
-        { src: '/assets/js/moment.twitter.js' },
-        { src: '/assets/js/APlayer.min.js' },
-        { src: '/assets/js/Meting.min.js' },
-        { src: '/assets/js/highlight.min.js' },
-        { src: '/assets/js/main.js' },
-        { src: '/assets/js/custom.js' },
     ]
-})
+});
+const { frontmatter, meta } = usePage();
+</script>
+<script lang="ts" client:load>
+let scripts = [
+    { src: '/assets/js/lazyload.min.js', type:'text/javascript' },
+    { src: '/assets/js/marked.min.js', type:'text/javascript' },
+    { src: '/assets/js/view-image.min.js', type:'text/javascript' },
+    { src: '/assets/js/moment.min.js', type:'text/javascript' },
+    { src: '/assets/js/moment.twitter.js', type:'text/javascript' },
+    { src: '/assets/js/APlayer.min.js', type:'text/javascript' },
+    { src: '/assets/js/Meting.min.js', type:'text/javascript' },
+    { src: '/assets/js/main.js', type:'text/javascript' },
+    { src: '/assets/js/custom.js', type:'text/javascript' },
+];
+scripts.forEach(ele => {
+    let script = document.createElement('script');
+    script.setAttribute('src',ele['src']);
+    script.setAttribute('type',ele['type']);
+    document.body.appendChild(script);
+});
 </script>
