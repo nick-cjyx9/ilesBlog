eddaf07d51fb1252fcd99ed46573e2d5a463e7a9
Nick Chen <1358390935@qq.com>
2024-05-04 12:55:18 +0800

    ci: debug

--- components.d.ts
+++ components.d.ts
@@ -20,7 +20,7 @@ declare module '@vue/runtime-core' {
     NotFound: typeof import('./src/components/NotFound.vue')['default']
     NotGenerateByAI: typeof import('./src/components/NotGenerateByAI.vue')['default']
     PostVisitorCounter: typeof import('./src/components/PostVisitorCounter.vue')['default']
-    ReadingLocationController: typeof import('./src/components/ReadingLocationController.vue')['default']
+    ReadingLocationController: typeof import('./src/components/readingLocationController.vue')['default']
     RenderFeed: typeof import('./node_modules/.pnpm/registry.npmmirror.com+@islands+feed@0.8.1_iles@0.9.5_vue@3.3.4/node_modules/@islands/feed/dist/render-feed')['RenderFeed']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
